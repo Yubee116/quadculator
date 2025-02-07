024114dd0eb266a3a484efd90083fcc7cbb5ed02
Ubong Nehemiah <ubongnehemiah@gmail.com>
2025-02-07 15:58:33 +0100

    time complexity optmizations

--- config/tax_calculator_constants.rb
+++ config/tax_calculator_constants.rb
@@ -1,3 +1,5 @@
+require 'set'
+
 module TaxCalculatorConstants
   EU_COUNTRIES_VAT_RATES = {
     'AT' => 0.20, 'BE' => 0.21, 'BG' => 0.20, 'HR' => 0.25, 'CY' => 0.19, 'CZ' => 0.21, 'DK' => 0.25, 'EE' => 0.20, 'FI' => 0.24, 'FR' => 0.20,
@@ -10,6 +12,6 @@ module TaxCalculatorConstants
 
   SPAIN_VAT = 0.21 # 21%
 
-  VALID_BUYER_TYPES = %i[individual company].freeze
-  VALID_TRANSACTION_TYPES = %w[good service digital onsite].freeze
+  VALID_BUYER_TYPES = Set.new(%i[individual company]).freeze
+  VALID_TRANSACTION_TYPES = Set.new(%w[good service digital onsite]).freeze
 end
--- lib/tax_calculator.rb
+++ lib/tax_calculator.rb
@@ -1,4 +1,5 @@
 require_relative '../config/tax_calculator_constants'
+require 'set'
 
 class TaxCalculator
   extend TaxCalculatorConstants
@@ -6,10 +7,11 @@ class TaxCalculator
   def self.calculate_tax(transaction)
     validate_transaction(transaction)
 
-    is_good = transaction[:transaction_type].include?('good')
-    is_service = transaction[:transaction_type].include?('service')
-    is_digital = transaction[:transaction_type].include?('digital')
-    is_onsite = transaction[:transaction_type].include?('onsite')
+    transaction_type = Set.new(transaction[:transaction_type])
+    is_good = transaction_type.include?('good')
+    is_service = transaction_type.include?('service')
+    is_digital = transaction_type.include?('digital')
+    is_onsite = transaction_type.include?('onsite')
 
     # Check if transaction is both a good and a service
     raise 'Invalid transaction: A transaction cannot be both a good and a service.' if is_good && is_service
@@ -18,6 +20,7 @@ class TaxCalculator
     raise 'Invalid transaction: A transaction cannot be both onsite and digital.' if is_digital && is_onsite
 
     taxed_transaction = transaction.dup
+    taxed_transaction[:transaction_type] = transaction_type
 
     if is_good
       apply_goods_tax(taxed_transaction)
@@ -29,7 +32,7 @@ class TaxCalculator
       end
     end
 
-    ensure_consistent_response(taxed_transaction)
+    taxed_transaction
   end
 
   class << self
@@ -44,11 +47,11 @@ class TaxCalculator
           transaction[:tax_rate] = EU_COUNTRIES_VAT_RATES[buyer_country]
         elsif buyer_type == :company
           transaction[:tax_rate] = 0
-          transaction[:transaction_type] << 'reverse charge'
+          transaction[:transaction_type].add('reverse charge')
         end
       else
         transaction[:tax_rate] = 0
-        transaction[:transaction_type] << 'export' if is_exportable
+        transaction[:transaction_type].add('export') if is_exportable
       end
     end
 
@@ -69,25 +72,17 @@ class TaxCalculator
 
     def validate_transaction(transaction)
       # check required fields
-      required_keys = %i[transaction_type buyer_country buyer_type]
-      missing_keys = required_keys.reject { |key| transaction.key?(key) }
+      required_keys = Set.new(%i[transaction_type buyer_country buyer_type])
+      missing_keys = required_keys - transaction.keys
       raise "Invalid transaction: Missing required fields - #{missing_keys.join(', ')}." unless missing_keys.empty?
 
-      # check transaction_type is array and elements are valid
-      unless transaction[:transaction_type].is_a?(Array) && transaction[:transaction_type].all? do |t|
-        VALID_TRANSACTION_TYPES.include?(t)
-      end
+      # check transaction_type contains at least one valid type
+      unless transaction[:transaction_type].any? { |t| VALID_TRANSACTION_TYPES.include?(t) }
         raise 'Invalid transaction: Unknown transaction type.'
       end
 
       # check buyer type is valid
       raise 'Invalid transaction: Unknown buyer type.' unless VALID_BUYER_TYPES.include?(transaction[:buyer_type])
-    end
-
-    def ensure_consistent_response(transaction)
-      transaction[:transaction_type] = Array(transaction[:transaction_type]).uniq
-      transaction[:tax_rate] ||= 0
-      transaction
     end
   end
 end
