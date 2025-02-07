8e6091e54e78920c681d3bf3ba5594e648a0e9af
Ubong Nehemiah <ubongnehemiah@gmail.com>
2025-02-07 12:51:47 +0100

    move constants to separate file and mark private methods

---
+++ config/tax_calculator_constants.rb
@@ -0,0 +1,15 @@
+module TaxCalculatorConstants
+  EU_COUNTRIES_VAT_RATES = {
+    'AT' => 0.20, 'BE' => 0.21, 'BG' => 0.20, 'HR' => 0.25, 'CY' => 0.19, 'CZ' => 0.21, 'DK' => 0.25, 'EE' => 0.20, 'FI' => 0.24, 'FR' => 0.20,
+    'DE' => 0.19, 'EL' => 0.24, 'HU' => 0.27, 'IE' => 0.23, 'IT' => 0.22, 'LV' => 0.21, 'LT' => 0.21, 'LU' => 0.17, 'MT' => 0.18, 'NL' => 0.21,
+    'PL' => 0.23, 'PT' => 0.23, 'RO' => 0.19, 'SK' => 0.23, 'SI' => 0.22, 'ES' => 0.21, 'SE' => 0.25
+  }
+
+  EU_COUNTRIES_VAT_RATES.each { |key, value| EU_COUNTRIES_VAT_RATES[key] = value.freeze }
+  EU_COUNTRIES_VAT_RATES.freeze
+
+  SPAIN_VAT = 0.21 # 21%
+
+  VALID_BUYER_TYPES = %i[individual company].freeze
+  VALID_TRANSACTION_TYPES = %w[good service digital onsite].freeze
+end
--- lib/tax_calculator.rb
+++ lib/tax_calculator.rb
@@ -1,17 +1,7 @@
+require_relative '../config/tax_calculator_constants'
+
 class TaxCalculator
-  EU_COUNTRIES_VAT_RATES = {
-    'AT' => 0.20, 'BE' => 0.21, 'BG' => 0.20, 'HR' => 0.25, 'CY' => 0.19, 'CZ' => 0.21, 'DK' => 0.25, 'EE' => 0.20, 'FI' => 0.24, 'FR' => 0.20,
-    'DE' => 0.19, 'EL' => 0.24, 'HU' => 0.27, 'IE' => 0.23, 'IT' => 0.22, 'LV' => 0.21, 'LT' => 0.21, 'LU' => 0.17, 'MT' => 0.18, 'NL' => 0.21,
-    'PL' => 0.23, 'PT' => 0.23, 'RO' => 0.19, 'SK' => 0.23, 'SI' => 0.22, 'ES' => 0.21, 'SE' => 0.25
-  }
-
-  EU_COUNTRIES_VAT_RATES.each { |key, value| EU_COUNTRIES_VAT_RATES[key] = value.freeze }
-  EU_COUNTRIES_VAT_RATES.freeze
-
-  SPAIN_VAT = 0.21 # 21%
-
-  VALID_BUYER_TYPES = %i[individual company].freeze
-  VALID_TRANSACTION_TYPES = %w[good service digital onsite].freeze
+  extend TaxCalculatorConstants
 
   def self.calculate_tax(transaction)
     validate_transaction(transaction)
@@ -42,54 +32,56 @@ class TaxCalculator
     taxed_transaction
   end
 
-  def self.apply_tax(transaction, is_exportable:)
-    buyer_country = transaction[:buyer_country]
-    buyer_type = transaction[:buyer_type]
+  class << self
+    def apply_tax(transaction, is_exportable:)
+      buyer_country = transaction[:buyer_country]
+      buyer_type = transaction[:buyer_type]
 
-    if buyer_country == 'ES'
-      transaction[:tax_rate] = SPAIN_VAT
-    elsif EU_COUNTRIES_VAT_RATES.key?(buyer_country)
-      if buyer_type == :individual
-        transaction[:tax_rate] = EU_COUNTRIES_VAT_RATES[buyer_country]
-      elsif buyer_type == :company
+      if buyer_country == 'ES'
+        transaction[:tax_rate] = SPAIN_VAT
+      elsif EU_COUNTRIES_VAT_RATES.key?(buyer_country)
+        if buyer_type == :individual
+          transaction[:tax_rate] = EU_COUNTRIES_VAT_RATES[buyer_country]
+        elsif buyer_type == :company
+          transaction[:tax_rate] = 0
+          transaction[:transaction_type] << 'reverse charge'
+        end
+      else
         transaction[:tax_rate] = 0
-        transaction[:transaction_type] << 'reverse charge'
+        transaction[:transaction_type] << 'export' if is_exportable
       end
-    else
-      transaction[:tax_rate] = 0
-      transaction[:transaction_type] << 'export' if is_exportable
-    end
-  end
-
-  def self.apply_goods_tax(transaction)
-    apply_tax(transaction, is_exportable: true)
-  end
-
-  def self.apply_digital_services_tax(transaction)
-    apply_tax(transaction, is_exportable: false)
-  end
-
-  def self.apply_onsite_services_tax(transaction)
-    service_location = transaction[:service_location]
-    raise 'Invalid transaction: Onsite service must have a service location.' if service_location.nil?
-
-    transaction[:tax_rate] = EU_COUNTRIES_VAT_RATES[service_location] || 0
-  end
-
-  def self.validate_transaction(transaction)
-    # check required fields
-    required_keys = %i[transaction_type buyer_country buyer_type]
-    missing_keys = required_keys.reject { |key| transaction.key?(key) }
-    raise "Invalid transaction: Missing required fields - #{missing_keys.join(', ')}." unless missing_keys.empty?
-
-    # check transaction_type is array and elements are valid
-    unless transaction[:transaction_type].is_a?(Array) && transaction[:transaction_type].all? do |t|
-      VALID_TRANSACTION_TYPES.include?(t)
-    end
-      raise 'Invalid transaction: Unknown transaction type.'
     end
 
-    # check buyer type is valid
-    raise 'Invalid transaction: Unknown buyer type.' unless VALID_BUYER_TYPES.include?(transaction[:buyer_type])
+    def apply_goods_tax(transaction)
+      apply_tax(transaction, is_exportable: true)
+    end
+
+    def apply_digital_services_tax(transaction)
+      apply_tax(transaction, is_exportable: false)
+    end
+
+    def apply_onsite_services_tax(transaction)
+      service_location = transaction[:service_location]
+      raise 'Invalid transaction: Onsite service must have a service location.' if service_location.nil?
+
+      transaction[:tax_rate] = EU_COUNTRIES_VAT_RATES[service_location] || 0
+    end
+
+    def validate_transaction(transaction)
+      # check required fields
+      required_keys = %i[transaction_type buyer_country buyer_type]
+      missing_keys = required_keys.reject { |key| transaction.key?(key) }
+      raise "Invalid transaction: Missing required fields - #{missing_keys.join(', ')}." unless missing_keys.empty?
+
+      # check transaction_type is array and elements are valid
+      unless transaction[:transaction_type].is_a?(Array) && transaction[:transaction_type].all? do |t|
+        VALID_TRANSACTION_TYPES.include?(t)
+      end
+        raise 'Invalid transaction: Unknown transaction type.'
+      end
+
+      # check buyer type is valid
+      raise 'Invalid transaction: Unknown buyer type.' unless VALID_BUYER_TYPES.include?(transaction[:buyer_type])
+    end
   end
 end
