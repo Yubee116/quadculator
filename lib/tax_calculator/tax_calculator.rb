6bc2e6d74ddff202665d8c2baf1c780bade14892
Ubong Nehemiah <ubongnehemiah@gmail.com>
2025-02-08 20:45:34 +0100

    separate transaction validation

--- lib/tax_calculator/tax_calculator.rb
+++ lib/tax_calculator/tax_calculator.rb
@@ -1,41 +1,30 @@
-require_relative '../config/tax_calculator_constants'
 require 'set'
+require_relative 'validators/transaction_validator'
+require_relative '../config/tax_constants'
 
 class TaxCalculator
   extend TaxCalculatorConstants
+  include TransactionValidator # Include the validator methods
 
   def self.calculate_tax(transaction)
-    validate_transaction(transaction)
+    validate_transaction(transaction) # Ensure transaction is valid
 
-    transaction_type = Set.new(transaction[:transaction_type])
-    is_good = transaction_type.include?('good')
-    is_service = transaction_type.include?('service')
-    is_digital = transaction_type.include?('digital')
-    is_onsite = transaction_type.include?('onsite')
+    # Duplicate transaction to prevent modifications to the original
+    transaction_copy = transaction.dup
+    transaction_copy[:transaction_type] = Set.new(transaction[:transaction_type])
 
-    # Check if transaction is both a good and a service
-    raise 'Invalid transaction: A transaction cannot be both a good and a service.' if is_good && is_service
-
-    # Check if transaction is neither a good nor a service
-    raise 'Invalid transaction: A transaction must be a good or a service.' if !is_good && !is_service
-
-    # Check if transaction is both onsite and digital
-    raise 'Invalid transaction: A transaction cannot be both onsite and digital.' if is_digital && is_onsite
-
-    taxed_transaction = transaction.dup
-    taxed_transaction[:transaction_type] = transaction_type
-
-    if is_good
-      apply_goods_tax(taxed_transaction)
-    elsif is_service
-      if is_digital
-        apply_digital_services_tax(taxed_transaction)
-      elsif is_onsite
-        apply_onsite_services_tax(taxed_transaction)
-      end
+    case transaction_copy[:transaction_type]
+    when transaction_copy[:transaction_type].include?('good')
+      apply_goods_tax(transaction_copy)
+    when transaction_copy[:transaction_type].include?('digital')
+      apply_digital_services_tax(transaction_copy)
+    when transaction_copy[:transaction_type].include?('onsite')
+      apply_onsite_services_tax(transaction_copy)
+    else
+      raise 'Invalid transaction: Unknown transaction type.'
     end
 
-    taxed_transaction
+    transaction_copy
   end
 
   class << self
@@ -71,21 +60,6 @@ class TaxCalculator
       raise 'Invalid transaction: Onsite service must have a service location.' if service_location.nil?
 
       transaction[:tax_rate] = EU_COUNTRIES_VAT_RATES[service_location] || 0
-    end
-
-    def validate_transaction(transaction)
-      # check required fields
-      required_keys = Set.new(%i[transaction_type buyer_country buyer_type])
-      missing_keys = required_keys - transaction.keys
-      raise "Invalid transaction: Missing required fields - #{missing_keys.join(', ')}." unless missing_keys.empty?
-
-      # check transaction_type contains at least one valid type
-      unless transaction[:transaction_type].any? { |t| VALID_TRANSACTION_TYPES.include?(t) }
-        raise 'Invalid transaction: Unknown transaction type.'
-      end
-
-      # check buyer type is valid
-      raise 'Invalid transaction: Unknown buyer type.' unless VALID_BUYER_TYPES.include?(transaction[:buyer_type])
     end
   end
 end
---
+++ lib/tax_calculator/validators/transaction_validator.rb
@@ -0,0 +1,44 @@
+require 'set'
+require_relative '../../config/tax_constants'
+
+module TransactionValidator
+  include TaxConstants
+
+  def validate_transaction(transaction)
+    transaction[:transaction_type] = Set.new(transaction[:transaction_type])
+
+    validate_transaction_fields(transaction)
+    validate_good_or_service(transaction)
+    validate_transaction_type_constraints(transaction)
+  end
+
+  private
+
+  def validate_transaction_fields(transaction)
+    required_keys = Set[:transaction_type, :buyer_country, :buyer_type]
+    missing_keys = required_keys - transaction.keys
+    raise "Invalid transaction: Missing required fields - #{missing_keys.to_a.join(', ')}." unless missing_keys.empty?
+
+    unless (transaction[:transaction_type] & VALID_TRANSACTION_TYPES).any?
+      raise 'Invalid transaction: Unknown transaction type.'
+    end
+
+    raise 'Invalid transaction: Unknown buyer type.' unless VALID_BUYER_TYPES.include?(transaction[:buyer_type])
+  end
+
+  def validate_good_or_service(transaction)
+    return if transaction[:transaction_type].intersect?(Set['good', 'service'])
+
+    raise 'Invalid transaction: A transaction must be either a good or a service.'
+  end
+
+  def validate_transaction_type_constraints(transaction)
+    if transaction[:transaction_type].superset?(Set['good', 'service'])
+      raise 'Invalid transaction: A transaction cannot be both a good and a service.'
+    end
+
+    return unless transaction[:transaction_type].superset?(Set['digital', 'onsite'])
+
+    raise 'Invalid transaction: A transaction cannot be both onsite and digital.'
+  end
+end
