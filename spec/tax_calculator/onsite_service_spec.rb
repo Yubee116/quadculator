1b563df68b3a60ee23c4b51525b1bb5bb29c92f4
Ubong Nehemiah <ubongnehemiah@gmail.com>
2025-02-21 19:02:17 +0100

    Update onsite_service_spec.rb
    - use constants
    - extract country label logic to function
    - update test generation logic

--- spec/tax_calculator/onsite_service_spec.rb
+++ spec/tax_calculator/onsite_service_spec.rb
@@ -1,45 +1,43 @@
-EU_COUNTRIES = %w[AT BE BG HR CY CZ DK EE FI FR DE EL HU IE IT LV LT LU MT NL PL PT RO SK SI ES SE].freeze
-BUYER_TYPES = %i[individual company].freeze
-BUYER_COUNTRIES = %w[ES FR RU].freeze
-
 describe 'Sale of  Onsite Services Tests' do
   include_context 'transaction setup'
 
+  EU_COUNTRIES_VAT_RATES = TaxCalculatorConstants::EU_COUNTRIES_VAT_RATES
+  BUYER_TYPES = TaxCalculatorConstants::VALID_BUYER_TYPES.to_a
+  BUYER_COUNTRIES = {
+    'Spain' => 'ES',
+    'France' => 'FR',
+    'Russia' => 'RU'
+  }.freeze
+
+  def self.country_label(country_code)
+    return '' if country_code == 'ES'
+
+    EU_COUNTRIES_VAT_RATES.key?(country_code) ? '(In EU)' : '(Outside EU)'
+  end
+
   context 'When transaction is an Onsite Service' do
     let(:transaction_type) { %w[service onsite] }
-    context 'provided in Spain' do
-      let(:service_location) { 'ES' }
 
-      BUYER_COUNTRIES.product(BUYER_TYPES).each do |country, buyer_type|
-        context "and buyer is a/an #{buyer_type} in #{country} #{if country == 'ES'
-                                                                   '(Spain)'
-                                                                 else
-                                                                   (EU_COUNTRIES.include?(country) ? '(In EU)' : '(Outside EU)')
-                                                                 end}" do
-          let(:buyer_type) { buyer_type }
-          let(:buyer_country) { country }
+    SERVICE_LOCATIONS = {
+      'Spain' => 'ES',
+      'Croatia' => 'HR',
+      'USA' => 'US'
+    }.freeze
 
-          it 'applies Spanish VAT' do
-            expect(TaxCalculator.calculate_tax(transaction)[:tax_rate]).to eq(0.21)
-          end
-        end
-      end
-    end
+    SERVICE_LOCATIONS.each do |service_country, service_country_code|
+      context "provided in #{service_country} #{country_label(service_country_code)}" do
+        let(:service_location) { service_country_code }
 
-    context 'provided in Croatia' do
-      let(:service_location) { 'HR' }
+        BUYER_COUNTRIES.to_a.product(BUYER_TYPES).each do |(buyer_country, buyer_country_code), buyer_type|
+          context "and buyer is a/an #{buyer_type} in #{buyer_country} #{country_label(buyer_country_code)}" do
+            let(:buyer_type) { buyer_type }
+            let(:buyer_country) { buyer_country_code }
 
-      BUYER_COUNTRIES.product(BUYER_TYPES).each do |country, buyer_type|
-        context "and buyer is a/an #{buyer_type} in #{country} #{if country == 'ES'
-                                                                   '(Spain)'
-                                                                 else
-                                                                   (EU_COUNTRIES.include?(country) ? '(In EU)' : '(Outside EU)')
-                                                                 end}" do
-          let(:buyer_type) { buyer_type }
-          let(:buyer_country) { country }
+            vat_rate = EU_COUNTRIES_VAT_RATES[service_country_code] || 0
 
-          it 'applies Croatian VAT' do
-            expect(TaxCalculator.calculate_tax(transaction)[:tax_rate]).to eq(0.25)
+            it "applies #{vat_rate * 100}% VAT" do
+              expect(TaxCalculator.calculate_tax(transaction)[:tax_rate]).to eq(vat_rate)
+            end
           end
         end
       end
