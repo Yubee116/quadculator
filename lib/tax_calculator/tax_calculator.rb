7e32ddc8fc4b3a157cac9fdf9b6125f95dd69c68
Ubong Nehemiah <ubongnehemiah@gmail.com>
2025-02-22 09:57:51 +0100

    Update tax_calculator.rb
    use normalizer.
    duplication moved to normalizer

--- lib/tax_calculator/tax_calculator.rb
+++ lib/tax_calculator/tax_calculator.rb
@@ -1,6 +1,7 @@
 require 'set'
 require_relative '../../config/tax_calculator_constants'
-require_relative 'validators/transaction_validator'
+require_relative 'utils/transaction_validator'
+require_relative 'utils/transaction_normalizer'
 require_relative 'strategies/goods_tax_strategy'
 require_relative 'strategies/digital_services_tax_strategy'
 require_relative 'strategies/onsite_services_tax_strategy'
@@ -8,12 +9,13 @@ require_relative 'strategies/onsite_services_tax_strategy'
 class TaxCalculator
 
   def self.calculate_tax(transaction)
-    TransactionValidator.validate_transaction(transaction) # Ensure transaction is valid
+    # Ensure transaction is in expected format
+    transaction_copy = TransactionNormalizer.normalize(transaction) 
 
-    # Duplicate transaction to prevent modifications to the original
-    transaction_copy = transaction.dup
-    transaction_copy[:transaction_type] = Set.new(transaction[:transaction_type])
+    # Ensure transaction is valid
+    TransactionValidator.validate_transaction(transaction_copy) 
 
+    # Apply tax
     if transaction_copy[:transaction_type].include?('good')
       GoodsTaxStrategy.apply_tax(transaction_copy)
     elsif transaction_copy[:transaction_type].include?('digital')
